2f21922cc4965af28f353ca1a7707c4d66dc6624
Zhiqing Tang <root@tzq.me>
2021-02-26 12:26:57 +0800

    Update autoBestTrace.sh

--- autoBestTrace.sh
+++ autoBestTrace.sh
@@ -3,10 +3,10 @@
 # apt -y install unzip
 
 # install besttrace
-if [ ! -f "besttrace" ]; then
-    wget https://github.com/zq/shell/raw/master/besttrace
+if [ ! -f "besttrace2021" ]; then
+    wget https://github.com/zq/shell/raw/master/besttrace2021
     # unzip besttrace4linux.zip
-    chmod +x besttrace
+    chmod +x besttrace2021
 fi
 
 ## start to use besttrace
@@ -18,13 +18,13 @@ next() {
 clear
 next
 
-ip_list=(14.215.116.1 101.95.120.109 117.28.254.129 113.207.25.138 119.6.6.6 183.192.160.3 183.221.253.100 202.112.14.151)
+ip_list=(14.215.116.1 101.95.120.109 117.28.254.129 113.207.25.138 119.6.6.6 120.204.197.126 183.221.253.100 202.112.14.151)
 ip_addr=(广州电信 上海电信 厦门电信 重庆联通 成都联通 上海移动 成都移动 成都教育网)
 # ip_len=${#ip_list[@]}
 
 for i in {0..7}
 do
 	echo ${ip_addr[$i]}
-	./besttrace -q 1 ${ip_list[$i]}
+	./besttrace2021 -q 1 ${ip_list[$i]}
 	next
 done
